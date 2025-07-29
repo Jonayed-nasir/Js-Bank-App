171d2ebd5412300bc322294f89d9d547d49e7cb7
jonayed-nasir <junayednasir073@gmail.com>
2025-07-29 14:03:56 +0600

    js  submitAction

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@
       document.getElementById('formSection').classList.remove('hidden');
     }
 
+    function submitAction() {
+      const amount = parseFloat(document.getElementById('amountInput').value);
+      const history = document.getElementById('history');
+      const balanceDisplay = document.getElementById('balance');
+
+      if (isNaN(amount) || amount <= 0) {
+        alert('Please enter a valid amount.');
+        return;
+      }
+
+    }
